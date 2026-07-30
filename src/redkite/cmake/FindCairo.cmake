f527691f78e670c9ce04f1a98b93da307a66e994
artexblack <64047705+artexblack@users.noreply.github.com>
2026-07-31 02:02:34 +0300

    Update FindCairo.cmake

--- src/redkite/cmake/FindCairo.cmake
+++ src/redkite/cmake/FindCairo.cmake
@@ -1,20 +1,44 @@
 # FindCairo.cmake
 
-find_path(CAIRO_INCLUDE_DIRS NAMES cairo/cairo.h)
-if (CMAKE_SYSTEM_NAME MATCHES Windows)
+if(CMAKE_SYSTEM_NAME MATCHES Windows)
+
     message(STATUS "Find Cairo for Windows/MSYS2/UCRT64...")
-    find_library(CAIRO_LIBRARIES NAMES cairo PATHS "/ucrt64/lib")
+
+    find_path(
+        CAIRO_INCLUDE_DIRS
+        NAMES cairo/cairo.h
+        PATHS
+            /ucrt64/include
+            /ucrt64/include/cairo
+    )
+
+    find_library(
+        CAIRO_LIBRARIES
+        NAMES cairo
+        PATHS
+            /ucrt64/lib
+    )
+
 else()
+
     message(STATUS "Find Cairo for GNU/Linux...")
+
+    find_path(CAIRO_INCLUDE_DIRS NAMES cairo/cairo.h)
+
     find_library(CAIRO_LIBRARIES NAMES cairo)
+
 endif()
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Cairo DEFAULT_MSG
-                                  CAIRO_INCLUDE_DIRS CAIRO_LIBRARIES)
 
-message("CMAKE_MODULE_PATH: ${CMAKE_MODULE_PATH}")
-message("CAIRO_INCLUDE_DIRS: ${CAIRO_INCLUDE_DIRS}")
-message("CAIRO_LIBRARIES: ${CAIRO_LIBRARIES}")
+find_package_handle_standard_args(
+    Cairo
+    DEFAULT_MSG
+    CAIRO_INCLUDE_DIRS
+    CAIRO_LIBRARIES
+)
+
+message(STATUS "CAIRO_INCLUDE_DIRS=${CAIRO_INCLUDE_DIRS}")
+message(STATUS "CAIRO_LIBRARIES=${CAIRO_LIBRARIES}")
 
 mark_as_advanced(CAIRO_INCLUDE_DIRS CAIRO_LIBRARIES)
